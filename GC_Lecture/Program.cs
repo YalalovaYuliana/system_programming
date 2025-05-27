eef151af78056cc413abb415b85680c56ba1649a
Yalalova Yuliana <ulianaualalova.ranepa@gmail.com>
2025-05-27 22:48:00 +0200

    GC lecture

---
+++ GC_Lecture/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GC_Lecture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Max generation: {GC.MaxGeneration}");
+            GarbageHelper hlp = new GarbageHelper();
+            Console.WriteLine($"Object generation: {GC.GetGeneration(hlp)}");
+            Console.WriteLine($"Busy memory (byte): {GC.GetTotalMemory(false)}");
+            hlp.MakeGarbage();
+            Console.WriteLine($"Busy memory (byte): {GC.GetTotalMemory(false)}");
+            GC.Collect();
+            Console.WriteLine($"Busy memory (byte): {GC.GetTotalMemory(false)}");
+            Console.WriteLine($"Object generation: {GC.GetGeneration(hlp)}");
+            GC.Collect();
+            Console.WriteLine($"Busy memory (byte): {GC.GetTotalMemory(false)}");
+            Console.WriteLine($"Object generation: {GC.GetGeneration(hlp)}");
+        }
+    }
+
+    class GarbageHelper
+    {
+        public void MakeGarbage()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                _ = new Person();
+            }
+        } 
+        class Person
+        {
+            string name;
+            string age;
+        }
+        
+    }
+}
---
+++ GC_Lecture/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("GC_Lecture")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GC_Lecture")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("1093668d-04e5-4e13-ad8e-5536e6a10d97")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

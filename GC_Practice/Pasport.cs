c1694dd2d6e7d890b1e62062d5eb454378af90da
Yalalova Yuliana <ulianaualalova.ranepa@gmail.com>
2025-05-30 23:33:00 +0200

    add GC practice

---
+++ GC_Practice/Pasport.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GC_Practice
+{
+    internal class Pasport : IDisposable
+    {
+        private string number;
+        public string Number
+        { 
+            get => number; 
+            set
+            {
+                foreach (var item in value)
+                {
+                    if (!Char.IsDigit(item))
+                    {
+                        throw new ArgumentException("Номер паспорта должен состоять только из цифр!");
+                    }
+                }
+                number = value;
+            } 
+        }
+        private string name;
+        public string Name 
+        { 
+            get => name; 
+            set
+            {
+                foreach (var item in value)
+                {
+                    if (!Char.IsLetter(item))
+                    {
+                        throw new ArgumentException("Имя должо состоять только из букв!");
+                    }
+                }
+
+                if (value.Length < 2)
+                {
+                    throw new Exception("Длина имени должна быть больше 2 символов!");
+                }
+
+                name = value;
+            }
+        }
+        public DateTime Date { get; set; }
+
+        ~Pasport() 
+        {
+            Console.WriteLine("Освобождение неуправляемых ресурсов");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Освобождение управляемых ресурсов");
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ GC_Practice/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace GC_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Multiplay();   
+            try
+            {
+                Pasport pasport = new Pasport();
+                pasport.Number = "12345";
+                pasport.Name = "Y";
+                pasport.Name = "XYZ";
+            } 
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            using(Pasport pasport = new Pasport())
+            {
+                pasport.Dispose();
+            }
+        }
+
+        static void Multiplay()
+        {
+            string primer = Console.ReadLine();
+
+            try
+            {
+                if (!Regex.IsMatch(primer, @"^\d+\*\d+(?:\*\d+)*$"))
+                {
+                    throw new Exception("Неверный ввод!");
+                }
+
+                string[] numbers = primer.Split('*');
+                long result = 1;
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    checked
+                    {
+                        result *= int.Parse(numbers[i]);
+                    }
+                }
+                Console.WriteLine($"Результат умножения: {result}");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Переполнение!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
